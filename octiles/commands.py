ce7359ea66cc3adefe170c7f1a0464106db45187
Oleg Dudnik <oleggelo86@gmail.com>
2026-07-07 15:41:00 +0300

    🦺 Add files validation to eq_command

--- octiles/commands.py
+++ octiles/commands.py
@@ -1,9 +1,11 @@
 from octiles.core import is_eq_files, dedup_files
-from octiles.validations import validate_paths_exists
+from octiles.validations import validate_paths_exists, validate_paths_are_files
 
 
 def eq_command(path_a: str, path_b: str):
     if not validate_paths_exists(path_a, path_b):
+        return
+    if not validate_paths_are_files(path_a, path_b):
         return
     print(is_eq_files(path_a, path_b))
 
