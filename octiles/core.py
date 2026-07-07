491d52972a5a99ffeeab8bf207de91019063cd01
Oleg Dudnik <oleggelo86@gmail.com>
2026-07-07 16:25:49 +0300

    🚧 Add diff_folders core function

--- octiles/core.py
+++ octiles/core.py
@@ -1,4 +1,13 @@
 import os
+from typing import Iterator
+from octiles.models import DiffResult
+
+
+def file_paths_generator(folder_path: str) -> Iterator[str]:
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+        if os.path.isfile(file_path):
+            yield file_path
 
 
 def is_eq_files(path_a: str, path_b: str) -> bool:
@@ -14,3 +23,26 @@ def dedup_files(path_a: str, path_b: str) -> bool:
     os.remove(path_b)
 
     return not os.path.exists(path_b)
+
+
+def diff_folders(folder_path_a: str, folder_path_b: str) -> DiffResult:
+
+    result = DiffResult.create_empty()
+
+    dict_a = {os.path.basename(path): path for path in file_paths_generator(folder_path_a)}
+    dict_b = {os.path.basename(path): path for path in file_paths_generator(folder_path_b)}
+
+    set_keys_a = set(dict_a.keys())
+    set_keys_b = set(dict_b.keys())
+
+    result.new = [dict_b[key] for key in set.difference(set_keys_b, set_keys_a)]
+    result.removed = [dict_a[key] for key in set.difference(set_keys_a, set_keys_b)]
+    for key in set.intersection(set_keys_a, set_keys_b):
+        file_path_a = dict_a[key]
+        file_path_b = dict_b[key]
+        if is_eq_files(file_path_a, file_path_b):
+            result.equals[key] = (file_path_a, file_path_b)
+        else:
+            result.unequals[key] = (file_path_a, file_path_b)
+
+    return result
