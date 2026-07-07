a207067c952d444644acc52ff589886cede8fc53
Oleg Dudnik <oleggelo86@gmail.com>
2026-07-07 15:34:56 +0300

    ♻️ Move validations functions to its own module

--- octiles/cmd.py
+++ octiles/cmd.py
@@ -1,32 +1,6 @@
 import os
 import sys
-from typing import Callable
-
-
-def validate_paths(predicate: Callable, path_a: str, path_b: str, error_message: str) -> bool:
-    is_valid = True
-
-    if not predicate(path_a):
-        is_valid = False
-        print(f'Path path_a is {error_message}: {path_a}')
-
-    if not predicate(path_b):
-        is_valid = False
-        print(f'Path path_b is {error_message}: {path_b}')
-
-    return is_valid
-
-
-def validate_paths_exists(path_a: str, path_b: str) -> bool:
-    return validate_paths(os.path.exists, path_a, path_b, 'not exists')
-
-
-def validate_paths_are_folders(path_a: str, path_b: str) -> bool:
-    return validate_paths(os.path.isdir, path_a, path_b, 'not folder')
-
-
-def validate_paths_are_files(path_a: str, path_b: str) -> bool:
-    return validate_paths(os.path.isfile, path_a, path_b, 'not file')
+from octiles.validations import validate_paths_exists
 
 
 def is_eq_files(path_a: str, path_b: str) -> bool:
