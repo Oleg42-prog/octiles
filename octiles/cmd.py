5eec27563de8e0b439b4bd6c4afec42c31a3840d
Oleg Dudnik <oleggelo86@gmail.com>
2026-07-07 15:31:01 +0300

    ♻️ Generalize validate_paths

--- octiles/cmd.py
+++ octiles/cmd.py
@@ -1,19 +1,24 @@
 import os
 import sys
+from typing import Callable
 
 
-def validate_paths(path_a: str, path_b: str) -> bool:
+def validate_paths(predicate: Callable, path_a: str, path_b: str, error_message: str) -> bool:
     is_valid = True
 
-    if not os.path.exists(path_a):
+    if not predicate(path_a):
         is_valid = False
-        print(f'Path path_a is not exists: {path_a}')
+        print(f'Path path_a is {error_message}: {path_a}')
 
-    if not os.path.exists(path_b):
+    if not predicate(path_b):
         is_valid = False
-        print(f'Path path_b is not exists: {path_b}')
+        print(f'Path path_b is {error_message}: {path_b}')
 
     return is_valid
+
+
+def validate_paths_exists(path_a: str, path_b: str) -> bool:
+    return validate_paths(os.path.exists, path_a, path_b, 'not exists')
 
 
 def is_eq_files(path_a: str, path_b: str) -> bool:
@@ -32,13 +37,13 @@ def dedup_files(path_a: str, path_b: str) -> bool:
 
 
 def eq_command(path_a: str, path_b: str):
-    if not validate_paths(path_a, path_b):
+    if not validate_paths_exists(path_a, path_b):
         return
     print(is_eq_files(path_a, path_b))
 
 
 def dedup_command(path_a: str, path_b: str):
-    if not validate_paths(path_a, path_b):
+    if not validate_paths_exists(path_a, path_b):
         return
     print(dedup_files(path_a, path_b))
 
